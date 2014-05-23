940ec08f3100bc3dc311010b8914e4a0e71b9f1c
Isaac Lean <isaaclean@creatibyte.com>
2014-05-22 22:09:32 -0700

    Some small changes

--- hw3/src/com/example/cmps121hw3/MainActivity.java
+++ hw3/src/com/example/cmps121hw3/MainActivity.java
@@ -34,50 +34,15 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
         		mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
     }
     
-    //Function crashes program at the moment
+    //setText() crashes program
     public void onSensorChanged(SensorEvent event){
     	if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
     		xAccel.setText("X: " + event.values[0]);
+    		yAccel.setText("Y: " + event.values[1]);
+    		zAccel.setText("Z: " + event.values[2]);
     	}
     }
     
     public void onAccuracyChanged(Sensor sensor, int accuracy){}
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
 
 }
